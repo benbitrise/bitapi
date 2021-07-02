e21b192435962aabcb3189a4b37fe89ef48c3281
Ben Boral <82456480+benbitrise@users.noreply.github.com>
2021-07-02 12:48:54 -0500

    linting and warning fixes (#2)

--- cli/bit-build.js
+++ cli/bit-build.js
@@ -1,23 +1,23 @@
 const commander = require('commander');
 
-const { config } = require('./config')
-const builds = require('../core/builds')
+const { config } = require('./config');
+const builds = require('../core/builds');
 
 const program = new commander.Command();
 
 program
-    .command('list')
-    .description('list builds for current app')
-    .action( async () => {
-        const token = await config.getToken()
-        const app = await config.getApp()
-        builds.list(token, app).then(list => {
-            console.log(JSON.stringify(list))
-        }).catch(e => {
-            console.error(e.message)
-        })
+  .command('list')
+  .description('list builds for current app')
+  .action(async () => {
+    const token = await config.getToken();
+    const app = await config.getApp();
+    builds.list(token, app).then((list) => {
+      console.log(JSON.stringify(list));
+    }).catch((e) => {
+      console.error(e.message);
     });
+  });
 
-program.command('artifact', 'interact with artifacts from builds')
+program.command('artifact', 'interact with artifacts from builds');
 
 program.parse(process.argv);
--- core/artifacts.js
+++ core/artifacts.js
@@ -1,24 +1,20 @@
-const apiPaginator = require('./api/paginator')
-const api = require('./api/client')
+const apiPaginator = require('./api/paginator');
+const api = require('./api/client');
 
-const listArtifacts = async (token, appSlug, buildSlug, params = {}) => {
-    const urlParams = new URLSearchParams()
-    const path = `apps/${appSlug}/builds/${buildSlug}/artifacts`
-    return apiPaginator.listItems(path, urlParams, token)
-}
+const listArtifacts = async (token, appSlug, buildSlug) => {
+  const urlParams = new URLSearchParams();
+  const path = `apps/${appSlug}/builds/${buildSlug}/artifacts`;
+  return apiPaginator.listItems(path, urlParams, token);
+};
 
 const getArtifactInfo = async (token, appSlug, buildSlug, artifactSlug) => {
-    const urlParams = new URLSearchParams()
-    const path = `apps/${appSlug}/builds/${buildSlug}/artifacts/${artifactSlug}`
-    const resp = await api.get(path, urlParams, token)
-    return resp.data.data
-}
+  const urlParams = new URLSearchParams();
+  const path = `apps/${appSlug}/builds/${buildSlug}/artifacts/${artifactSlug}`;
+  const resp = await api.get(path, urlParams, token);
+  return resp.data.data;
+};
 
 module.exports = {
-    list: (token, appSlug, buildSlug) => {
-        return listArtifacts(token, appSlug, buildSlug)
-    },
-    getInfo: (token, appSlug, buildSlug, artifactSlug) => {
-        return getArtifactInfo(token, appSlug, buildSlug, artifactSlug)
-    }
-}
+  list: listArtifacts,
+  getInfo: getArtifactInfo,
+};
