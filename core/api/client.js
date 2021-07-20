72901143a2ac14c5da36dced76d78fcc1a3ab441
Ben Boral <ben.boral@bitrise.io>
2021-07-20 12:54:41 -0500

    rate limits to 3 requests per second

--- core/api/client.js
+++ core/api/client.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
+const rateLimit = require('axios-rate-limit');
 
 const baseUrl = 'https://api.bitrise.io/v0.1/';
 
 module.exports = {
   get: (path, params, token) => {
+    const http = rateLimit(axios.create(), { maxRPS: 3 })
     const url = new URL(`${baseUrl}${path}`);
 
     params.forEach((value, key) => {
@@ -11,6 +13,6 @@ module.exports = {
     });
     const header = { Authorization: `${token}` };
     const config = { headers: header };
-    return axios.get(url.toString(), config);
+    return http.get(url.toString(), config);
   },
 };
